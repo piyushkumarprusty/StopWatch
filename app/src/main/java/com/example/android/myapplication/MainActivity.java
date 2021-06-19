dea05de741fc6cacab9d127b31be78b1437ff519
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-19 18:07:03 +0530

    Connecting elements in main file and animation effect

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvSplash , tvSubSplash ;
+    Button btnget;
+    Animation atg;
+    ImageView ivSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnget = findViewById(R.id.btnget);
+        ivSplash = findViewById(R.id.ivSplash);
+
+        // Animation
     }
 }
