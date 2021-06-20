902160b925370c43bcde8f5ee2ca6bc5c68a79bb
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-20 17:13:58 +0530

    button search func

--- app/src/main/java/com/example/android/myapplication/StopWatch.java
+++ app/src/main/java/com/example/android/myapplication/StopWatch.java
@@ -3,12 +3,25 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class StopWatch extends AppCompatActivity {
+
+    Button btnstart;
+    ImageView icanchor;
+    Animation roundingalone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+
+        btnstart =findViewById(R.id.btnget);
+        icanchor = findViewById(R.id.icanchor);
+
+        //Load animations
+
     }
 }
